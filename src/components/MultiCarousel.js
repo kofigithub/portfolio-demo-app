ef7571f525bb3e0067bfbe3fcd3ff4b91e074deb
kofigithub <hoocha88@yahoo.com>
2023-07-05 13:14:04 +0000

    update site

--- src/components/MultiCarousel.js
+++ src/components/MultiCarousel.js
@@ -1,8 +1,47 @@
 import React, { Component } from "react";
-import ReactDOM from "react-dom";
+//import ReactDOM from "react-dom";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import Header from "./Header";
+import image1 from '../images/image1.jpg';
+import image2 from '../images/image2.jpg';
+import image3 from '../images/image3.jpg';
+import image4 from '../images/image4.jpg';
+import image5 from '../images/image5.jpg';
+
+
+const data = [
+  {
+    label: "Image 1",
+    alt: "image1",
+    urlToImage:image1,
+	siteUrl:"www.infoscanghana.com"
+  },
+  {
+    label: "Image 2",
+    alt: "image2",
+    urlToImage:image2,
+	siteUrl:"www.infoscanghana.com"
+  },
+  {
+    label: "Image 3",
+    alt: "image3",
+    urlToImage: image3,
+	siteUrl:"www.infoscanghana.com"
+  },
+  {
+    label: "Image 4",
+    alt: "image4",
+    urlToImage:image4,
+	siteUrl:"www.infoscanghana.com"
+  },
+  {
+    label: "Image 5",
+    alt: "image5",
+    urlToImage:image5,
+	siteUrl:"www.infoscanghana.com"
+  }
+];
 
 const responsive = {
   superLargeDesktop: {
@@ -26,24 +65,19 @@ const responsive = {
 class MultiCarousel extends Component {
   state = {
     loading: false,
-    data: [],
-    headline: []
+    data: []
   };
 
   componentDidMount() {
     this.setState({ loading: true });
     console.log("app mounted");
-    fetch(
-      "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=8ee8c21b20d24b37856fc3ab1e22a1e5"
-    )
-      .then(data => data.json())
-      .then(data =>
-        this.setState({ data: data.articles, loading: false }, () =>
-          console.log(data.articles)
+    
+        this.setState({ data: data, loading: false }, () =>
+          console.log(data.label)
         )
-      );
+      
   }
-
+  
   render() {
     return (
 	<div>
@@ -55,18 +89,35 @@ class MultiCarousel extends Component {
             "loading..."
           ) : (
             <div>
-              <Carousel responsive={responsive}>
-                {this.state.data.map((post, indx) => {
+              <Carousel 
+			  additionalTransfrom={0} 
+			  arrows 
+			  autoPlay={true}
+			  autoPlaySpeed={3000}
+			  centerMode={true}
+			  className=""
+			  containerClass="container-with-dots"
+			  dotListClass=""
+			  draggable
+			  focusOnSelect={false}
+			  infinite
+			  itemClass=""
+			  keyBoardControl
+			  minimumTouchDrag={80}
+			  renderButtonGroupOutside={false}
+			  renderDotsOutside={false}
+			  responsive={responsive}>
+                {this.state.data.map((post, index) => {
                   return (
-                    <div className="card text-left mt-5" key={indx}>
+                    <div className="card text-left mt-5" key={index}>
                       <img
                         style={{ height: "100px" }}
                         src={post.urlToImage}
                         alt="Alt text"
                       />
                       <div className="card-body">
-                        <h5 className="card-title">{post.title}</h5>
-                        <p className="card-text">{post.description}</p>
+                        <h5 className="card-title">{post.label}</h5>
+                        <p className="card-text">{post.label}</p>
                         <a
                           href={post.url}
                           target="_blank"
